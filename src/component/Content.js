e11313c711a32c8eade6f7dc044be3a560dc97c1
medoum <mohamedoum2430@gmail.com>
2023-11-15 16:46:52 +0100

    adding content and fixing some details

--- src/component/Content.js
+++ src/component/Content.js
@@ -2,7 +2,67 @@ import React from 'react'
 
 const Content = () => {
   return (
-    <div>Content</div>
+    
+<div class="my-10">
+<h1 className='text-center font-bold'>Style Gallery</h1>
+<div class="flex flex-row w-full mt-6 pt-6 space-x-0 space-y-6 px-6">
+      <div class="bg-gray-50 card flex flex-row mx-2 rounded-xl my-2">
+        <div class="card-detail flex flex-col justify-between">
+          <div class="top flex flex-row justify-between mx-4 my-6 font-semibold text-gray-500">
+         
+          </div>
+          <div class="middle mx-3">
+            <h1>adidas serie 1</h1>
+          <img src="../../image1.jpeg" alt="Girl in a jacket" width="800" height="800" />
+          <p>800Dh</p>
+          </div>
+         
+        </div>
+      </div>
+      <div class="bg-gray-50 card flex flex-row mx-2 rounded-xl my-2">
+        <div class="card-detail flex flex-col justify-between">
+          <div class="top flex flex-row justify-between mx-4 my-6 font-semibold text-gray-500">
+         
+          </div>
+          <div class="middle mx-3">
+            <h1>adidas serie 1</h1>
+          <img src="../../image1.jpeg" alt="Girl in a jacket" width="800" height="800" />
+          <p>800Dh</p>
+          </div>
+         
+        </div>
+      </div>
+      <div class="bg-gray-50 card flex flex-row mx-2 rounded-xl my-2">
+        <div class="card-detail flex flex-col justify-between">
+          <div class="top flex flex-row justify-between mx-4 my-6 font-semibold text-gray-500">
+         
+          </div>
+          <div class="middle mx-3">
+            <h1>adidas serie 1</h1>
+          <img src="../../image1.jpeg" alt="Girl in a jacket" width="800" height="800" />
+          <p>800Dh</p>
+          </div>
+         
+        </div>
+      </div>
+      <div class="bg-gray-50 card flex flex-row mx-2 rounded-xl my-2">
+        <div class="card-detail flex flex-col justify-between">
+          <div class="top flex flex-row justify-between mx-4 my-6 font-semibold text-gray-500">
+         
+          </div>
+          <div class="middle mx-3">
+            <h1>adidas serie 1</h1>
+          <img src="../../image1.jpeg" alt="Girl in a jacket" width="800" height="800" />
+          <p>800Dh</p>
+          </div>
+         
+        </div>
+      </div>
+   
+</div>
+</div>
+
+
   )
 }
 
